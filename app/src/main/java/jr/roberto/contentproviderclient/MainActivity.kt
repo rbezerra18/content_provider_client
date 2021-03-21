b7700656ef910dfb1100d4a111f9d8b8403f1f83
Roberto Júnior <rbezerra18@hotmail.com>
2021-03-21 14:10:35 -0300

    Criar ClientAdapter.kt e client_item.xml. Implementar métodos onCreatViewHolder e onBindViewHolder.

--- app/src/main/java/jr/roberto/contentproviderclient/MainActivity.kt
+++ app/src/main/java/jr/roberto/contentproviderclient/MainActivity.kt
@@ -1,11 +1,23 @@
 package jr.roberto.contentproviderclient
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var notesRecycler: RecyclerView
+    lateinit var noteRefreshButton: FloatingActionButton
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notesRecycler = findViewById(R.id.client_list)
+        noteRefreshButton = findViewById(R.id.client_btn_refresh)
+
+        noteRefreshButton.setOnClickListener {  }
     }
 }
